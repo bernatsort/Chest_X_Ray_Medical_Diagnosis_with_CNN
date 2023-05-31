a13c5223a130a8c215fd2923d8d0449f471c9c50
bernatsort <bernatsr14@gmail.com>
2023-05-31 21:18:36 +0200

    first commit

---
+++ code.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import pandas as pd
+import numpy as np 
+import seaborn as sns
+import matplotlib
+import matplotlib.pyplot as plt 
+
+
+# packages versions
+print('Python: {}'.format(sys.version))
+print('Pandas: {}'.format(pd.__version__))
+print('Numpy: {}'.format(np.__version__))
+print('Matplotlib: {}'.format(matplotlib.__version__))
+print('Seaborn: {}'.format(sns.__version__))
+
+
+
+
