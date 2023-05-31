9709290094c6adfa6c4a448eea399f6390ddad21
Jordi Rodeiro Boliart <jordi.rodeiro@students.salle.url.edu>
2023-05-31 21:23:35 +0200

    Body py

---
+++ Body_Classification.py
@@ -0,0 +1,32 @@
+import pydicom
+import matplotlib.pyplot as plt
+import os
+
+# Path to the directory containing DICOM files
+directory = 'data/train'
+
+def load_data(url: str):
+    pass
+
+def find_images(url: str):
+    # Iterate over each file in the directory
+    for filename in os.listdir(url):
+        file_path = os.path.join(url, filename)
+        
+        if filename.endswith('.dcm'):  # Only process DICOM files
+            
+            # Load the DICOM file
+            ds = pydicom.dcmread(file_path)
+            
+            # Access the pixel data
+            pixel_data = ds.pixel_array
+            
+            # Display the image
+            plt.imshow(pixel_data, cmap=plt.cm.gray)
+            plt.axis('off')  # Optional: remove axes
+            plt.title(filename)  # Optional: display filename as the title
+            plt.show()
+        elif os.path.isdir(file_path):
+            find_images(file_path)
+
+find_images(directory)
