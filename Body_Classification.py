66e5c118bfdb58367d3637ad6a3e818638456a7f
Jordi Rodeiro Boliart <jordi.rodeiro@students.salle.url.edu>
2023-06-05 20:37:04 +0200

    parameter change

--- Body_Classification.py
+++ Body_Classification.py
@@ -113,7 +113,7 @@ def complete_code(main_dir: str, csv_dir: str):
     #print(cm)
     
     # Create a figure and axis
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(25, 15))
 
     # Create a heatmap of the confusion matrix
     sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", ax=ax)
@@ -124,7 +124,8 @@ def complete_code(main_dir: str, csv_dir: str):
     ax.set_title("Confusion Matrix")
 
     # Show the plot
-    plt.show()
+    #plt.show()
+    plt.savefig("CM.png")
 
 def complete_code_2(main_dir: str, csv_dir: str):
     tags_df = load_data(csv_dir)
@@ -213,15 +214,15 @@ def classification_VGG(main_dir: str, csv_dir: str):
                 
                 #cv2.imshow("Title", image_array.flatten())
                 #cv2.waitKey(0)
-                
+        
     for i in range(len(image_names)):
         for row in tags_df.index:
             if tags_df.loc[row, "SOPInstanceUID"] in image_names[i]:
-                image_labels.append(tags_df.loc[row, "Target"].split(' ')[0])
-                tags_df.loc[row, "Target"] = tags_df.loc[row, "Target"].split(' ')[0]
+                new_label = tags_df.loc[row, "Target"].split(' ')[0]
+                image_labels.append(new_label)
+                tags_df.loc[row, "Target"] = new_label
 
-    print(tags_df)
-    print(tags_df['Target'].unique())
+    descriptive_analysis(tags_df)
     
     # Convert the image list and labels to NumPy arrays
     X = np.array(image_list)
@@ -287,13 +288,13 @@ def classification_VGG(main_dir: str, csv_dir: str):
     
     # Validations checkpoints and Early stopping
     checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_accuracy', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
-    early = EarlyStopping(monitor='val_accuracy', min_delta=0, patience=20, verbose=1, mode='auto')
+    early = EarlyStopping(monitor='val_accuracy', min_delta=0, patience=10, verbose=1, mode='auto')
 
     print(X_train.shape)
 
     model.summary()
-    #hist = model.fit(traindata, steps_per_epoch=25, validation_data=testdata, validation_steps=10, epochs=5, callbacks=[checkpoint, early])
-    hist = model.fit(traindata, steps_per_epoch=7, validation_data=testdata, validation_steps=5, epochs=50)
+    hist = model.fit(traindata, steps_per_epoch=5, validation_data=testdata, validation_steps=5, epochs=50, callbacks=[checkpoint, early])
+    #hist = model.fit(traindata, steps_per_epoch=7, validation_data=testdata, validation_steps=5, epochs=50)
     
     # Plot train history
     plt.plot(hist.history["accuracy"])
@@ -312,7 +313,158 @@ def classification_VGG(main_dir: str, csv_dir: str):
     plt.ylabel("Loss")
     plt.xlabel("Epoch")
     plt.legend(["loss","Validation Loss"])
-    plt.savefig("Accuracy.png")
+    plt.savefig("Loss.png")
+    plt.clf()
+    
+def classification_VGG_bin(main_dir: str, csv_dir: str):
+    
+    tags_df = load_data(csv_dir)
+    
+    target_size=(224,224,3)
+
+    # Load the images and collect image names, tags, and labels
+    image_list = []
+    image_names = []
+    image_labels = []
+    
+    for root, dirs, files in os.walk(main_dir):
+        for file in files:
+            if file.endswith(".dcm"):
+                dcm_path = os.path.join(root, file)
+                dcm = pydicom.dcmread(dcm_path)
+                image_array = dcm.pixel_array
+                
+                resized_image = cv2.resize(image_array, target_size[:2])
+                if len(target_size) == 3 and resized_image.ndim == 2:
+                    #resized_image = np.expand_dims(resized_image, axis=-1)
+                    resized_image = np.expand_dims(resized_image, axis=-1)
+                    resized_image = np.concatenate([resized_image] * 3, axis=-1)  # Convert grayscale to RGB
+                
+                image_list.append(resized_image)
+                #image_list.append(image_array.flatten())
+                image_names.append(file)
+                
+                #image = np.array(image_array).shape
+                #print(image)
+                
+                #cv2.imshow("Title", image_array.flatten())
+                #cv2.waitKey(0)
+        
+    for i in range(len(image_names)):
+        for row in tags_df.index:
+            if tags_df.loc[row, "SOPInstanceUID"] in image_names[i]:
+                new_label = tags_df.loc[row, "Target"].split(' ')[0]
+                
+                if int(new_label) == 3 or int(new_label) == 20:
+                    new_label = 0
+                elif int(new_label) == 4 or int(new_label) == 14:
+                    new_label = 1
+                else:
+                    new_label = 1
+                    
+                image_labels.append(new_label)
+                tags_df.loc[row, "Target"] = new_label
+
+    descriptive_analysis(tags_df)
+        
+    # Convert the image list and labels to NumPy arrays
+    X = np.array(image_list)
+    print(X.shape)
+    #X = np.array([tf.image.resize(np.expand_dims(image, axis=0), target_size).numpy() for image in image_list])
+
+    #X = np.array([tf.image.resize(image, target_size).numpy() for image in image_list])
+    y = np.array(image_labels)
+
+    # Perform label encoding for the target labels
+    label_encoder = LabelEncoder()
+    onehot_encoder = OneHotEncoder(sparse=False)
+
+    y_encoded = label_encoder.fit_transform(y)
+    y_encoded = onehot_encoder.fit_transform(y_encoded.reshape(-1, 1))
+    
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.8, random_state=0)
+    
+    tr_datagen = ImageDataGenerator()
+    te_datagen = ImageDataGenerator()
+    
+    # Set the batch size
+    batch_size = 32
+
+    # Resize train data
+    #X_train = np.array([tf.image.resize(image, target_size).numpy() for image in X_train])
+    #X_test = np.array([tf.image.resize(image, target_size).numpy() for image in X_test])
+
+    # Create data generators
+    traindata = tr_datagen.flow(X_train, y_train, batch_size=batch_size)
+    testdata = te_datagen.flow(X_test, y_test, batch_size=batch_size)
+
+    # Layers
+    model = Sequential()
+    model.add(Conv2D(input_shape=(224,224,3),filters=64,kernel_size=(3,3),padding="same", activation="relu"))
+    model.add(Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    model.add(Flatten())
+    model.add(Dense(units=4096,activation="relu"))
+    model.add(Dense(units=4096,activation="relu"))
+    #model.add(Dense(units=2, activation="softmax"))
+    model.add(Dense(units=2, activation="softmax"))
+
+    # Optimizer
+    opt = Adam(learning_rate=0.001)
+    model.compile(optimizer=opt, loss=keras.losses.categorical_crossentropy, metrics=['accuracy'])
+    
+    # Validations checkpoints and Early stopping
+    checkpoint = ModelCheckpoint("vgg16_2.h5", monitor='val_accuracy', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
+    early = EarlyStopping(monitor='val_accuracy', min_delta=0, patience=10, verbose=1, mode='auto')
+
+    print(X_train.shape)
+
+    model.summary()
+    hist = model.fit(traindata, steps_per_epoch=5, validation_data=testdata, validation_steps=5, epochs=50, callbacks=[checkpoint, early])
+    #hist = model.fit(traindata, steps_per_epoch=7, validation_data=testdata, validation_steps=5, epochs=50)
+    
+    # Plot train history
+    plt.clf()
+    fig, ax = plt.subplots(figsize=(25, 15))
+    plt.plot(hist.history["accuracy"])
+    plt.plot(hist.history['val_accuracy'])
+    plt.title("model accuracy")
+    plt.ylabel("Accuracy")
+    plt.xlabel("Epoch")
+    plt.legend(["Accuracy","Validation Accuracy"])
+    plt.savefig("Accuracy_v2.png")
+    
+    # Plot train history
+    plt.clf()
+    fig, ax = plt.subplots(figsize=(25, 15))
+    plt.plot(hist.history['loss'])
+    plt.plot(hist.history['val_loss'])
+    plt.title("model loss")
+    plt.ylabel("Loss")
+    plt.xlabel("Epoch")
+    plt.legend(["loss","Validation Loss"])
+    plt.savefig("Loss_v2.png")
+    plt.clf()
+    
+def descriptive_analysis(tags_df):
+    
+    print(tags_df['Target'].value_counts())
     
 
 train_csv_file = "Body_Parts_Dataset/train.csv"
@@ -321,6 +473,8 @@ train_csv_file = "Body_Parts_Dataset/train.csv"
 directory = 'Body_Parts_Dataset/train'
 #images_list = find_images(directory)
 
-classification_VGG(directory, train_csv_file)
-complete_code(directory, train_csv_file)
+#complete_code(directory, train_csv_file)
 #complete_code_2(directory, train_csv_file)
+
+classification_VGG(directory, train_csv_file)
+classification_VGG_bin(directory, train_csv_file)
